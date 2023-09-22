18a742be21b631ead938ca314ce200f3dad72207
bishal16 <mahathirbishal@gmail.com>
2023-09-23 02:11:23 +0600

    added new solution

--- 1174. Immediate Food Delivery II.sql
+++ 1174. Immediate Food Delivery II.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+# Write your MySQL query statement 
 # select 
 #     round(sum(case when order_date = cus_pref_date then 1 else 0 end)*100/count(*),2)
 #     as immediate_percentage
